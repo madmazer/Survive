1f4ee3a2526adf5731f82cb89dc711938f235029
Jason Griggs <jd.griggs86@gmail.com>
2015-05-06 18:03:25 +0930

    0.0.1
    First Build Test Plugins

---
+++ brains.js
@@ -0,0 +1,41 @@
+cordova plugin add org.apache.cordova.vibration
+var survivors = 20;
+
+$('.topNav').html(survivors + " Survivors");
+//resources
+//currentVal-
+var morale=[50];
+var food=[10,25];
+var scrapmetal=[0];
+var health=[100];
+
+
+var resources=[morale, food, scrapmetal,health];
+
+
+
+//Functions that effect resources
+
+//food
+function checkMorale(){
+navigator.vibrate(3000);
+
+};
+
+//AD NETWORK
+
+var ad1="<a href='http://trkur4.com/69233/19174?&i=162439' target='_blank' ><img src='http://pixxur.com/69233/162439-500x500.jpg' /></a>";
+var ad2="<a href='http://trkur4.com/69233/19174?&i=162468' target='_blank' ><img src='http://pixxur.com/69233/162468-480x700.jpg' /></a>";
+var ad3="<a href='http://trkur4.com/69233/19167?&i=162226' target='_blank' ><img src='http://pixxur.com/69233/162226-1024x500.png' /></a>";
+var ad4="<a href='http://trkur3.com/69233/18354?&i=147999' target='_blank' ><img src='http://pixxur.com/69233/147999-320x480.jpg' /></a>";
+var ad5="<a href='http://trkur3.com/69233/18354?&i=147994' target='_blank' ><img src='http://pixxur.com/69233/147994-320x480.jpg' /></a>";
+
+
+function randomAd(){
+    var advertStatics=[ad1,ad2,ad3,ad4,ad5];
+var adrand=chance.integer({min: 0, max: 4});
+var displayAd=advertStatics[adrand];
+$('#adverts').html(displayAd);
+
+
+}
