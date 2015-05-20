23061ede881ec1f210602d2caaec87dc3f7d7ccf
Mazer <jd.griggs86@gmail.com>
2015-05-20 15:43:30 +0930

    0.0.10

--- brains.js
+++ brains.js
@@ -4,21 +4,22 @@ var availableWorkers = survivors;
 $('.topNav').html(survivors + " Survivors");
 //resources
 //currentVal-
-var morale = [50,"Morale"];
-var food = [100,"Food"];
-var scrapmetal = [0,"Scrap"];
-var health = [80,"Health"];
+var morale = [50, "Morale"];
+var food = [100, "Food", 0, 0];
+var scrapmetal = [0, "Scrap"];
+var health = [80, "Health"];
 
 
-var resources = [food, scrapmetal,morale, health];
+var resources = [food, scrapmetal, morale, health];
 
 var scavangers = [0, 0.2];
-var foragers = [0, 1];
+var foragers = [0, 0.9];
 var farmers = [0, 0.2];
 var repairs = [0, 3.2];
 var doctors = [0, 0.4];
 
 var workers = [scavangers, foragers, farmers, repairs, doctors]
+
 
 
 
@@ -28,10 +29,9 @@ var workers = [scavangers, foragers, farmers, repairs, doctors]
 function adWorker(parm) {
     if (availableWorkers > 0) {
         var currentTotal = workers[parm][0];
-        workers[parm][0] = currentTotal+1;
+        workers[parm][0] = currentTotal + 1;
         availableWorkers = availableWorkers - 1;
         console.log(availableWorkers);
-
         console.log('Working ' + currentTotal);
 
     }
@@ -39,17 +39,17 @@ function adWorker(parm) {
 }
 
 function minusWorker(parm) {
-    
-    var currentTotal = workers[parm][0];
-    workers[parm][0] = currentTotal-1;
-    availableWorkers = availableWorkers + 1;
-     console.log(availableWorkers);
 
-        console.log('Working ' + currentTotal);
-    
-    
+    var currentTotal = workers[parm][0];
+    workers[parm][0] = currentTotal - 1;
+    availableWorkers = availableWorkers + 1;
+    console.log(availableWorkers)
+    console.log('Working ' + currentTotal);
+
+
 
 }
+
 
 //TEST FUNCTION FOR AUTO ADDING RESPOURCES
 
@@ -58,53 +58,55 @@ function minusWorker(parm) {
 //foodout
 
 setInterval(function () {
-    
-    var foodUse=0.4;
-    var foodout=parseInt(survivors*foodUse);
-    food[0]=food[0]-foodout;
+
+    var foodUse = 0.7;
+    var foodout = parseInt(survivors * foodUse);
+    food[0] = food[0] - foodout;
+    resources[0][2]=foodout;
     rundisplay();
-    }, 1000);
+}, 1000);
 
 //foodin
 
 setInterval(function () {
-    
-    var foodFarms=workers[2][0]*workers[2][1];
-    var foodScav=workers[0][0]*workers[0][1];
-    var foodForag=workers[1][0]*workers[1][1];
-    var totalFoodIn=(foodFarms+foodForag+foodScav)
-   resources[0][0]=resources[0][0]+parseInt(totalFoodIn);
-    console.log("Total Food in "+totalFoodIn);
+
+    var foodFarms = workers[2][0] * workers[2][1];
+    var foodScav = workers[0][0] * workers[0][1];
+    var foodForag = workers[1][0] * workers[1][1];
+    var totalFoodIn = (foodFarms + foodForag + foodScav)
+    resources[0][0] = resources[0][0] + parseInt(totalFoodIn);
+    console.log("Total Food in " + totalFoodIn);
+        resources[0][1]=totalFoodIn;
+
     rundisplay();
-    }, 1000);
+}, 1000);
+
+///FOOD TOTAL
+
+
 //SCRAP
 //ScrapIn
-setInterval(function () {
-    
-    var foodFarms=workers[2][0]*workers[2][1];
-    var foodScav=workers[0][0]*workers[0][1];
-    var foodForag=workers[1][0]*workers[1][1];
-    var totalFoodIn=(foodFarms+foodForag+foodScav)
-   resources[0][0]=resources[0][0]+parseInt(totalFoodIn);
-    console.log("Total Food in "+totalFoodIn);
-    rundisplay();
-    }, 1000);
+
 
 
 //DISPLAY///////////////////////////////////////////////////
 
-function rundisplay(){for (i = 0; i < resources.length; i++) { 
-    
-    var selector=i;
-    $( "#disp"+selector+" h4" ).html( resources[i][1]+ " "+resources[i][0] );
+function rundisplay() {
+    for (i = 0; i < resources.length; i++) {
+       var totalfoodrate= resources[i][2] - resources[i][1]
+        var selector = i;
+        $("#disp_tblname" + selector).html(resources[i][1]);
+        $("#disp_tblrate" + selector).html(resources[0][1]-resources[0][2]);
+        $("#disp_tblowned" + selector).html(resources[i][0]);
 
-}}
+    }
+}
 
 
-$(document).ready(function(){
-    $("button .btn-even").attr("width","500px");
-        
-    });
+$(document).ready(function () {
+    $("button .btn-even").attr("width", "500px");
+
+});
 /*//AD NETWORK
 
 // select the right Ad Id according to platform
